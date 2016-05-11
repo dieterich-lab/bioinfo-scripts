07d1f1be64b9f1add25bc1dcda82fe14d6cbc8d6
Tobias Jakobi <tjakobi@med.uni-heidelberg.de>
2016-05-11 09:51:44 +0200

    Cleaned up line breaks

--- slurm_star_paired.sh
+++ slurm_star_paired.sh
@@ -5,7 +5,7 @@
 # @Email:  tobias.jakobi@med.uni-heidelberg.de
 # @Project: University Hospital Heidelberg, Section of Bioinformatics and Systems Cardiology
 # @Last modified by:   tjakobi
-# @Last modified time: Friday, May 6, 2016 4:15 PM
+# @Last modified time: Friday, May 6, 2016 4:22 PM
 # @License: CC BY-NC-SA
 
 #SBATCH -n 1
@@ -32,30 +32,11 @@ target=`expr ${2/_R1/} : '\(.*\)\..*\.'`
 
 
 
-STARlong --genomeDir /data/projects/departments/Christoph_Dieterich/circRNA_evolution/tobias/genomes/{=s/_.*//=}/\
---runThreadN 40\
---readFilesIn /data/projects/departments/Christoph_Dieterich/circRNA_evolution/tobias/names/{..}.1.gz\
-/data/projects/departments/Christoph_Dieterich/circRNA_evolution/tobias/names/{..}.2.gz\
---sjdbGTFfile /data/projects/departments/Christoph_Dieterich/circRNA_evolution/tobias/genomes/{=s/_.*//=}/{=s/_.*//=}.gtf\
---readFilesCommand zcat --outFileNamePrefix /data/projects/departments/Christoph_Dieterich/circRNA_evolution/tobias/STAR/{...}/\
---outSAMtype BAM SortedByCoordinate --outSAMstrandField intronMotif \& ::: *.1.gz
-
-
-
-
-
-
-
-
-
-# load the bowtie2 module
-module load bowtie2
-
-# SAM output goes to /dev/null
-# run on 20 CPUs
-# set fixed seed
-# memory mapped IO for multiple instances
-# display timing information
-# write bz2 unmapping reads [== no rRNA] to target dir
-
-bowtie2 -x $1 -1 $2 -2 $3 -S /dev/null --threads 20 --mm --seed 1337 --time --un-conc-bz2 $4
+STARlong --genomeDir \
+--runThreadN 40\ # 40CPUs
+--readFilesIn $1 $2\
+--sjdbGTFfile mes/{=s/_.*//=}/{=s/_.*//=}.gtf\
+--readFilesCommand bzcat\ # use bz2 input files
+--outFileNamePrefix $4\
+--outSAMtype BAM SortedByCoordinate
+--outSAMstrandField intronMotif
