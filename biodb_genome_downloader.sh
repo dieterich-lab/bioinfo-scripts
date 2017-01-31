65e85f63e7409619a7f030791c48454b7804b90a
Tobias Jakobi <Tobias.Jakobi@med.uni-heidelberg.de>
2017-01-31 13:23:47 +0100

    Re-worked cluster version of biodb-downlader

--- biodb_genome_downloader.sh
+++ biodb_genome_downloader.sh
@@ -7,8 +7,6 @@
 # @Date:   Tuesday, July 26, 2016 2:00 PM
 # @Email:  tobias.jakobi@med.uni-heidelberg.de
 # @Project: University Hospital Heidelberg, Section of Bioinformatics and Systems Cardiology
-# @Last modified by:   tjakobi
-# @Last modified time: Wednesday, July 27, 2016 8:00 PM
 # @License: CC BY-NC-SA
 
 ## updated for HD cluster by Tobias / 14.04.2016
@@ -19,14 +17,14 @@
 # Level2 folder = WBcell235_78
 # reference genome (fasta and index) = WBcel235.dna.toplevel
 # reference GTF = WBcel235.78.gtf
-# reference GTF index = caenorhabditis_elegans/WBcell235_78/GTF_index/
+# reference GTF index = caenorhabditis_elegans/WBcell235_78/gtf_index/
 # cuffcompare fixed GTF = cuffcmp_GTF.WBcel235.78.gtf
 # other cuffcompare output files = cuffcmp.results.tar.bz2
-# cuffcompare fixed GTF index = caenorhabditis_elegans/WBcell235_78/GTF_cuffcmp_GTF_index/
+# cuffcompare fixed GTF index = caenorhabditis_elegans/WBcell235_78/GTF_cuffcmp_gtf_index/
 # chromosomes fasta = caenorhabditis_elegans/WBcell235_78/fasta
 
 # example tophat options:
-# --transcriptome-index $references_directory/caenorhabditis_elegans/WBcell235_78/GTF_cuffcmp_GTF_index
+# --transcriptome-index $references_directory/caenorhabditis_elegans/WBcell235_78/GTF_cuffcmp_gtf_index
 # for the reference indexed genome use $references_directory/caenorhabditis_elegans/WBcell235_78/bowtie2/WBcel235.dna.toplevel
 
 # example cufflinks options:
@@ -47,84 +45,264 @@ fi
 
 cd $organism
 
+latest_ensembl_release=$(curl -s ftp://ftp.ensembl.org/pub/current_README | grep Release | cut -f3 -d " ")
+
 ## Choice: either we get the latest release (default) or the user specifies
 ## the release
-
+release=0
 release_force=$2
+echo $2
+alias sbatch='sbatch --mail-type=END,FAIL,TIME_LIMIT_80  --mail-user=tobias.jakobi@med.uni-heidelberg.de'
+alias srun='srun --mail-type=END,FAIL,TIME_LIMIT_80  --mail-user=tobias.jakobi@med.uni-heidelberg.de'
 
-if [ $release_force ] ; then
+if [ -n "$release_force" ] && [ -z "$3" ] && [ "$release_force" == "pre" ] ; then # get the pre release
 
-  release="release-$2"
+  release=$2
+  release_str="$release"
 
-  printf "ENSEMBL release $release manually selected\n"
+  printf "ENSEMBL release $release manually selected ($latest_ensembl_release is current)\n"
 
   # check the latest release
-  gtf_import=$(curl -s -l ftp://ftp.ensembl.org/pub/$release/gtf/$organism/ | grep gtf.gz)
+  gtf_import=$(curl -s -l ftp://ftp.ensembl.org/pub/pre/gtf/$organism/ | grep gtf)
   releaseA=$(echo $gtf_import | cut -f2 -d.)
   releaseB=$(echo $gtf_import | cut -f3 -d.)
   sep=_
   release_code=$releaseA$sep$releaseB
+  if [ "${release_code/$release}" != "$release_code" ]
+  then
+    echo "Genome release includes ENSEMBL build";
+  else
+    echo "Genome release does not include ENSEMBL build, adding";
+    release_code=$release_code"_"$release
+  fi
+
+  release=$release
+  printf "Genome ID is $release_code\n"
+  unset $release_force
+
+elif [ -n "$release_force" ]  && [ -z "$3" ] && [ "$release_force" != "pre" ] ; then # get the selected release
+
+  release=$2
+  release_str="release-$release"
+
+  printf "ENSEMBL release $release manually selected ($latest_ensembl_release is current)\n"
+
+  # check the latest release
+  gtf_import=$(curl -s -l ftp://ftp.ensembl.org/pub/release-$release/gtf/$organism/ | grep gtf.gz)
+  releaseA=$(echo $gtf_import | cut -f2 -d.)
+  releaseB=$(echo $gtf_import | cut -f3 -d.)
+  sep=_
+  release_code=$releaseA$sep$releaseB
+  if [ "${release_code/$release}" != "$release_code" ]
+  then
+    echo "Genome release includes ENSEMBL build";
+  else
+    echo "Genome release does not include ENSEMBL build, adding";
+    release_code=$release_code"_"$release
+  fi
+
+  release=release-$release
   printf "Genome ID is $release_code\n"
 
-else
+elif [ -z "$3" ] ; then # just get the last ensembl release
   printf "Get genome releases...\n"
 
-  # check the latest release
-  gtf_import=$(curl -s -l ftp://ftp.ensembl.org/pub/current_gtf/$organism/ | grep gtf.gz)
+  printf "Latest ENSEMBL release is: $latest_ensembl_release\n"
+  gtf_import=$(curl -s -l ftp://ftp.ensembl.org/pub/release-$latest_ensembl_release/gtf/$organism/ | grep gtf.gz)
   releaseA=$(echo $gtf_import | cut -f2 -d.)
   releaseB=$(echo $gtf_import | cut -f3 -d.)
-  release="release-$releaseB"
+  release=release-$latest_ensembl_release
   sep=_
   release_code=$releaseA$sep$releaseB
+  if [ "${release_code/$latest_ensembl_release}" != "$release_code" ]
+  then
+    echo "Genome release includes ENSEMBL build";
+  else
+    echo "Genome release does not include ENSEMBL build, adding";
+    release_code=$release_code"_"$latest_ensembl_release
+  fi
 
   printf "Genome ID is $release_code\n"
+elif [ -n "$3" ] ;then # no ensembl, take the supplied fasta and gtf file
+
+	printf "Manual node selected.\n"
+	printf "Organism name: $1\n"
+	printf "Release name: $2\n"
+	printf "FASTA file: $3\n"
+	printf "GTF file: $4\n"
+
+	organism=$1
+	release_code=$2
+	orginal=$4
+	
+	if [ -d "$release_code" ]; then
+	  printf "Directory $release_code already exists, exiting program\n"
+	  exit
+	fi
+
+	mkdir $release_code
+	cd $release_code
+	cp $4 . -av
+	cp $3 . -av
+
+        original=$(ls *.fa)
+        toplevel=${original#".fa"}
+        toplevel=${toplevel%".fa"}
 fi
 
-mkdir $release_code
-cd $release_code
 
 
 
-printf "Downloading data...\n"
-
-wget -nc ftp://ftp.ensembl.org/pub/$release/fasta/$organism/dna/*.dna.*
-wget -nc ftp://ftp.ensembl.org/pub/$release/gtf/$organism/*.gtf.gz
-
-rm -v *abinitio.gtf.gz
-rm -v *.chr.gtf.gz
-
-printf "Uncompressing...\n"
-
-parallel -v -j 40 srun gunzip {} ::: *.gz
-
-printf "Housekeeping...\n"
-
-namep1=$(echo $gtf_import | cut -f1 -d.)
-
-for file in $(ls $namep1.*); do
-nname=${file#"$namep1."}
-nname=${nname%"$namep1."}
-mv -v $file $nname; done
 
 
-mkdir -v chromosomes
-for file in $(ls *chromosome*); do
-if [ ${file} != chromosomes: ]; then
-chrp1=$(echo $file | cut -f4 -d.)
-chrp2=$(echo $file | cut -f5 -d.)
-sep=.
-chr=$chrp1$sep$chrp2
 
-mv -v ${file} chromosomes/${chr}; fi; done
-mv -v *nonchromosomal* chromosomes/nonchromosomal.fa
+if  [ -z "$3" ] ; then
 
-original=$(ls *dna.toplevel.fa)
-toplevel=${original#".fa"}
-toplevel=${toplevel%".fa"}
+	if [ -d "$release_code" ]; then
+	  printf "Directory $release_code already exists, exiting program\n"
+	  exit
+	fi
+
+	printf "Downloading data...\n"
+	
+	mkdir $release_code
+	cd $release_code
+
+
+
+	if [ "$release_force" = "pre" ] ; then
+		# we have to test if we have .dna. files, otherwise we take the _RM files
+		files=$(curl -s -l ftp://ftp.ensembl.org/pub/pre/fasta/dna/$organism/ | grep ".dna\.")
+		
+		if [ -n "$files" ]
+		then 
+			wget -nc -nv ftp://ftp.ensembl.org/pub/pre/fasta/dna/$organism/*.dna.toplevel.fa.gz
+		else	
+			wget -nc -nv ftp://ftp.ensembl.org/pub/pre/fasta/dna/$organism/*.dna_rm.toplevel.fa.gz
+		fi
+	else
+		wget -nc -nv ftp://ftp.ensembl.org/pub/$release/fasta/$organism/dna/*.dna.toplevel.fa.gz
+		wget -nc -nv ftp://ftp.ensembl.org/pub/$release/fasta/$organism/dna/*.dna.primary_assembly.fa.gz
+	fi
+
+	wget -nc -nv ftp://ftp.ensembl.org/pub/$release/gtf/$organism/*.gtf.gz
+
+	rm -v *abinitio.gtf.gz
+	rm -v *.chr.gtf.gz
+	rm -v *.patch_hapl_scaff.gtf.gz	
+
+	printf "Uncompressing...\n"
+
+	parallel -v -j 40 srun gunzip {} ::: *.gz
+
+	printf "Housekeeping...\n"
+
+	namep1=$(echo $gtf_import | cut -f1 -d.)
+
+	for file in $(ls $namep1.*); do
+	nname=${file#"$namep1."}
+	nname=${nname%"$namep1."}
+	mv -v $file $nname; done
+
+
+	#mkdir -v chromosomes
+	#for file in $(ls *chromosome*); do
+	#if [ ${file} != chromosomes: ]; then
+	#chrp1=$(echo $file | cut -f4 -d.)
+	#chrp2=$(echo $file | cut -f5 -d.)
+	#sep=.
+
+	#chr=$(echo $file | egrep -o '[[:alnum:]]*.fa')
+
+	#mv -v ${file} chromosomes/${chr}; fi; done
+	#mv -v *nonchromosomal* chromosomes/nonchromosomal.fa
+
+	original=$(ls *dna.primary_assembly.fa)
+	# since 09/2016 we use primary assembly instead of toplevel
+	#original=$(ls *dna.toplevel.fa)
+
+	# but we still use top level as fallback if no assembly exists, i.e. pre builds
+	if [ -z "$original" ] ; then
+		original=$(ls *dna.toplevel.fa)
+	fi
+
+
+	mv -v ${original} ${release_code}.fa 
+
+	original=$(ls ${release_code}.fa)
+
+	toplevel=${original#".fa"}
+	toplevel=${toplevel%".fa"}
+fi 
+
+
+
+printf "Fasta indexing\n"
+
+module load samtools
+echo "#!/bin/bash
+samtools faidx ${original}
+which samtools" > samtools.sh
+chmod 770 samtools.sh;
+sbatch -J "Fasta index: $organism / $release" -o samtools.log samtools.sh
+
+
+
+printf "Running RepeatModeler & RepeatMasker\n"
+
+module load repeatmasker
+module load repeatmodeler
+
+mkdir -v repeat_masker
+cd repeat_masker
+full_path=$(pwd)
+ln -vs ../${original} ${original}
+echo "#!/bin/bash
+BuildDatabase -name $organism -engine ncbi ${original}
+RepeatModeler -pa 10 -engine ncbi -database $organism
+MODEL_PATH=\$(grep consensi.fa.classified repeat_masker.log | cut -f3 -d ' ')
+RepeatMasker -s -u -ace -xm -html -norna --gff -pa 10 -lib \$MODEL_PATH/consensi.fa.classified ${original}  
+# RepeatMasker -s -u -ace -xm -html -norna --gff -pa 10 -lib consensi.fa.classified ${original} 
+# RepeatMasker -s -u -ace -xm -html -norna --gff -pa 10 -species "${organism/_/ }" ${original}
+ln -vs repeat_masker/${original}.out.gff ../${original::(-3)}_repeatmasker.gtf
+tar -cvf repeat_masker.tar \$MODEL_PATH
+pbzip2 -v -p40 repeat_masker.tar
+rm -v RM_* -rf
+which RepeatMasker" > repeat_masker.sh
+chmod 770 repeat_masker.sh;
+sbatch -p long --cpus-per-task=40 --mem=250GB -J "RepeatModeler: $organism / $release" -o repeat_masker.log repeat_masker.sh
+cd ..
+
+
+
+module load blast
+mkdir blast
+cd blast
+full_path=$(pwd)
+ln -s ../${original} ${original}
+echo "#!/bin/bash
+makeblastdb -in ${original} -out ${original/\.fa/}  -dbtype nucl -logfile ../blast.log " > blast.sh
+chmod 770 blast.sh;
+sbatch -p general --cpus-per-task=2 --mem=10GB -J "makebelastdb: $organism / $release" -o /dev/null blast.sh
+cd ..
+
+module load hisat2
+mkdir hisat2
+cd hisat2
+full_path=$(pwd)
+ln -s ../${original} ${original}
+echo "#!/bin/bash
+hisat2-build $original $toplevel
+which hisat2" > hisat2.sh
+chmod 770 hisat2.sh;
+sbatch -p general --cpus-per-task=2 --mem=10GB -J "HISAT2 index: $organism / $release" -o hisat2.log hisat2.sh
+cd ..
+
 
 # BWA index creation
 
 printf "Generating BWA index\n"
+
 
 module load bwa
 mkdir bwa
@@ -135,7 +313,7 @@ echo "#!/bin/bash
 bwa index -a bwtsw -p ${full_path}/${original::(-3)} ${original}
 which bwa" > bwa.sh
 chmod 770 bwa.sh;
-sbatch -J "bwa index $organism" -o bwa.log bwa.sh
+sbatch -p general --cpus-per-task=2 --mem=10GB -J "BWA index: $organism / $release" -o bwa.log bwa.sh
 cd ..
 
 
@@ -147,7 +325,7 @@ module load bowtie2
 mkdir bowtie2
 cd bowtie2
 ln -vs ../${original} ${original}
-srun -J "bowtie2 index $organism" -e bowtie2.err  -o bowtie2.log -c 2 --mem 40000 bowtie2-build-s $original $toplevel
+srun -J "Bowtie2 index: $organism / $release" -e bowtie2.err  -o bowtie2.log -c 2 --mem 10G bowtie2-build-s $original $toplevel
 
 cd ..
 pwd
@@ -164,10 +342,10 @@ full_path=$(pwd)
 ln -s ../${original} ${original}
 ln -s ../${gtf} ${gtf}
 echo "#!/bin/bash
-STAR --runMode genomeGenerate --genomeDir ${full_path} --genomeFastaFiles ${original} --runThreadN 40 --sjdbGTFfile ${gtf} --sjdbOverhang 100 --limitGenomeGenerateRAM 240000000000
+STAR --runMode genomeGenerate --genomeDir ${full_path} --genomeFastaFiles ${original} --runThreadN 40 --limitGenomeGenerateRAM 240000000000
 which STAR" > star.sh;
 chmod 770 star.sh;
-sbatch -J "STAR index: $organism"  --cpus-per-task=40 --mem=256GB -o star.log star.sh
+sbatch -p long -J "STAR index: $organism / $release"  --cpus-per-task=40 --mem=250G -o star.log star.sh
 cd ..
 
 
@@ -177,69 +355,31 @@ printf "Fixing GTF $gtf...\n"
 
 module load cufflinks
 which cuffcompare >> cuffcompare.log
-srun -J "cuffcompare fix $organism" -c 20 --mem=40000 -o cuffcompare.log cuffcompare -V -CG -s chromosomes -r $gtf $gtf
-mv -v cuffcmp.combined.gtf cuffcmp_GTF.$gtf
-srun -o tar.log -J "cuffcompare taring" tar -cvf cuffcmp.results.tar cuffcmp.* --remove-files
-srun -o pbzip2.log -J "cuffcompare bzipping" -c 20 --mem=4000 pbzip2 -p20 cuffcmp.results.tar
+srun -J "Cuffcompare: $organism / $release" -c 20 --mem=40G -o /dev/null cuffcompare -V -CG -s ${original} -r $gtf $gtf
+mv -v cuffcmp.combined.gtf tmp.gtf
+rm -v cuffcmp.*
+mv -v tmp.gtf cuffcmp.$gtf
+#srun -o tar.log -J "cuffcompare taring" tar -cvf cuffcmp.results.tar cuffcmp.* --remove-files
+#srun -o pbzip2.log -J "cuffcompare bzipping" -c 20 --mem=4000 pbzip2 -p20 cuffcmp.results.tar
 
 # Generate TOPHAT transcriptome indexes
 
 printf "Indexing cuffcompare GTF\n"
 
 module load tophat
-mkdir cuffcmp_GTF_index
-srun -J "TopHat2 $organism"  -e tophat2.err -o tophat2.log -c 40 --mem=40GB tophat2 -p 40 -G cuffcmp_GTF.$gtf --transcriptome-index cuffcmp_GTF_index bowtie2/$toplevel
+mkdir cuffcmp_gtf_index
+srun -J "TopHat2: $organism / $release"  -e tophat2.err -o tophat2.log -c 40 --mem=40GB tophat2 -p 40 -G cuffcmp.$gtf --transcriptome-index cuffcmp_gtf_index bowtie2/$toplevel
 which tophat2 >> tophat2.log
-mv tophat2.log tophat2.err cuffcmp_GTF_index/
+mv tophat2.log tophat2.err cuffcmp_gtf_index/
 rm -r tophat_out
 
 
 printf "Indexing GTF\n"
 
-mkdir GTF_index
-srun -J "TopHat2 $organism"  -e tophat2_gtf_index.err -o tophat2_gtf_index.log -c 40 --mem=40GB tophat2 -p 40 -G $gtf --transcriptome-index GTF_index bowtie2/$toplevel
+mkdir gtf_index
+srun -J "TopHat2 GTF index: $organism / $release"  -e tophat2_gtf_index.err -o tophat2_gtf_index.log -c 40 --mem=40GB tophat2 -p 40 -G $gtf --transcriptome-index gtf_index bowtie2/$toplevel
 which tophat2 >> tophat2_gtf_index.log
-mv tophat2_gtf_index.log tophat2_gtf_index.err cuffcmp_GTF_index/
+mv tophat2_gtf_index.log tophat2_gtf_index.err cuffcmp_gtf_index/
 rm -r tophat_out
 
 
-exit
-
-else
-
-cd $release
-
-printf "
-You already have this release in:
-"
-pwd
-printf "
-Press enter to list components.
-"
-read
-ls
-printf "
-Do you wish to add more components? (y/n)
-"
-read answer
-
-# If the user wants to add more components then they should be implemented into the script.
-
-if [ $answer == 'n' ]; then exit;
-
-elif [ $answer == 'y' ]; then
-
-printf "
-Please contact me: tobias.jakobi@med.uni-heidelberg.de | (+49) 6221 56 39126
-";
-
-else
-
-printf "
-Exiting.. you can only type y or n
-"
-exit
-
-fi; fi
-
-exit
