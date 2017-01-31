34ecffa83b6d8169ccd59e38182c538bcbb18271
Tobias Jakobi <Tobias.Jakobi@med.uni-heidelberg.de>
2017-01-31 13:39:00 +0100

    Changes for flexbar 2.7

--- slurm_flexbar_paired.sh
+++ slurm_flexbar_paired.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
 # @Author: Tobias Jakobi <tjakobi>
-# @Date:   Tuesday, May 3, 2016 6:51 PM
 # @Email:  tobias.jakobi@med.uni-heidelberg.de
 # @Project: University Hospital Heidelberg, Section of Bioinformatics and Systems Cardiology
-# @Last modified by:   tjakobi
-# @Last modified time: Wednesday, May 4, 2016 2:35 PM
 # @License: CC BY-NC-SA
 
 #SBATCH -n 1
 #SBATCH -N 1
-#SBATCH -c 10
-#SBATCH --mem=10G
+#SBATCH -c 20
+#SBATCH --mem=1G
 #SBATCH -J "flexbar paired"
-
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT_80
+#SBATCH --mail-user=tobias.jakobi@med.uni-heidelberg.de
 
 # check if we have 3 arguments
-if [ ! $# == 3 ]; then
-  echo "Usage: $0 [Read 1 file] [Read 2 file] [target dir e.g. /tmp/]"
+if [ ! $# == 4 ]; then
+  echo "Usage: $0 [Read 1 file] [Read 2 file] [target dir e.g. /tmp/] [R1 marker, e.g. _R1]"
   exit
 fi
 
 # $1 -> Read 1
-# $2 -> Read 3
+# $2 -> Read 2
 # $3 -> Target directory
+
 
 # remove the file extension and potential "R1" markings
 # (works for double extension, e.g. .fastq.gz)
-target=`expr ${1/_R1/} : '\(.*\)\..*\.'`
+target=`expr ${1/$4/} : '\(.*\)\..*\.'`
 
 # load the flexbar module
 module load flexbar
@@ -38,4 +37,7 @@ module load flexbar
 # no uncalled bases
 # quality min phred 28
 # use sanger quality values (i.e. Illumina 1.9+ encoding)
-flexbar -r $1 -p $2 -t $3/$target  -n 10 -z BZ2 -m 30 -u 0 -q 28 -a /biosw/flexbar/Adapter.fa -f sanger
+#flexbar -r $1 -p $2 -t $3/$target  -n 20 -z GZ -m 30 -u 0 -qt 28 -a /biosw/flexbar/Adapter.fa -qf sanger
+#flexbar -r $1 -p $2 -t $3/$target  -n 20 -z GZ -m 30 -u 0 -q 28 -a /biosw/flexbar/Adapter.fa -f sanger -j 
+#flexbar -r $1 -p $2 -t $3/$target  -n 20 -z GZ -m 30 -u 0  -q TAIL -qt 28 -as "AGATCGGAAGAG" -qf sanger -j
+flexbar -r $1 -p $2 -t $3/$target  -n 20 -z GZ -m 30 -u 0  -q TAIL -qt 28 -a /biosw/flexbar/Adapter.f -qf sanger -j
