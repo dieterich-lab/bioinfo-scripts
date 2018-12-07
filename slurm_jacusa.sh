0e2684a277d15c31e50e150a90c9bb0088e476f1
Tobias Jakobi <tobias.jakobi@med.uni-heidelberg.de>
2018-12-07 14:16:15 +0100

    Updating JACUSA call

--- slurm_jacusa.sh
+++ slurm_jacusa.sh
@@ -19,8 +19,4 @@ export set1=$1
 export set2=$2
 export output=$3
 
-echo java -Xmx100g -jar /biosw/jacusa/1.2.3/build/JACUSA_v1.2.3.jar call-2 -s -c 5 -P FR-FIRSTSTRAND,FR-FIRSTSTRAND -p 10 -W 1000000 -F 1024 --filterNM_1 5 --filterNM_2 5 -T 1 -a D,M,Y -r ${output} ${set1} ${set2}
-
-
-#java -Xmx200g -jar /biosw/jacusa/1.2.0/build/JACUSA_v1.2.0.jar call-2 -s -c 5 -P FR-FIRSTSTRAND,FR-FIRSTSTRAND -p 40 -W 1000000 -F 1024 --filterNM_1 5 --filterNM_2 5 -T 1 -a D,M,Y -r ${output} ${set1} ${set2}
-java -Xmx200g -jar /biosw/jacusa/1.2.3/build/JACUSA_v1.2.3.jar call-2 -s -c 5 -P FR-FIRSTSTRAND,FR-FIRSTSTRAND -p 40 -W 1000000 --filterNM_1 5 --filterNM_2 5 -F 1024 -a D,M,Y -r ${output} ${set1} ${set2}
+java -Xmx250g -jar /biosw/jacusa/1.2.3/build/JACUSA_v1.2.3.jar call-2 -s -c 5 -P RF-FIRSTSTRAND,RF-FIRSTSTRAND -p 20 -W 1000000 --filterNM_1 5 --filterNM_2 5 -F 1024 -a D,M,Y -r ${output} ${set1} ${set2}
