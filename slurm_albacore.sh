ab82eba5400f300c892a78957f0be1ce3a0c1e12
Tobias Jakobi <tobias.jakobi@med.uni-heidelberg.de>
2018-12-07 14:08:19 +0100

    Changing to 40 cores for broad cluster use

--- slurm_albacore.sh
+++ slurm_albacore.sh
@@ -7,19 +7,24 @@
 
 #SBATCH -n 1
 #SBATCH -N 1
-#SBATCH -c 60 
-#SBATCH --mem=950G
+#SBATCH -c 40 
+#SBATCH --mem=250G
 #SBATCH -J "albacore"
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT_80
 #SBATCH --mail-user=tobias.jakobi@med.uni-heidelberg.de
+#SBATCH -p long
 
 #module unload python3*
 module load albacore
 
 # check if we have 2 arguments
-if [ ! $# == 2 ]; then
-  echo "Usage: $0 [input] [output]"
+if [ ! $# == 3 ]; then
+  echo "Usage: $0 [Input folder (recursive)] [Output folder] [Kit] "
   exit
 fi
 
-read_fast5_basecaller.py -k SQK-RNA001 -f FLO-MIN107 -s ${2} -o fastq,fast5 -r -t 60 -i ${1}
+# we set the flow cell to 106:
+#Christoph Dieterich @cdieterich 5:14 PM
+#Aha, in that case you can practically always start with FLO-MIN106... 107 is for that 1D squared chemistry which we never use
+
+read_fast5_basecaller.py -k ${3} -f FLO-MIN106 -s ${2} -o fastq -r -q 0 -t 40 -i ${1}
