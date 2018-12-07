1518843031a0d7f0ad819096135d2f88c6fdf47c
Tobias Jakobi <tobias.jakobi@med.uni-heidelberg.de>
2018-12-07 14:03:51 +0100

    Updated with multi CPU parameter for new 7z version

--- slurm_7z_gz_test.sh
+++ slurm_7z_gz_test.sh
@@ -6,10 +6,12 @@
 
 #SBATCH -n 1
 #SBATCH -N 1
-#SBATCH -c 2
-#SBATCH --mem=2G
-#SBATCH -J "gzip test"
+#SBATCH -c 10
+#SBATCH --mem=20G
+#SBATCH -J "7zip verify"
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT_80
 #SBATCH --mail-user=tobias.jakobi@med.uni-heidelberg.de
 
-7z t $1
+module load p7zip
+
+7z t -mmt20 $1
