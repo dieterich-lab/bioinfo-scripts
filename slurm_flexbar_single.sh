3cbad11e66c786b7b0fcd60f0bd5981693a53904
Tobias Jakobi <tobias.jakobi@med.uni-heidelberg.de>
2018-12-07 14:15:20 +0100

    Fixing SE flexbar call

--- slurm_flexbar_single.sh
+++ slurm_flexbar_single.sh
@@ -1,40 +1,42 @@
 #!/bin/bash
 
 # @Author: Tobias Jakobi <tjakobi>
-# @Date:   Tuesday, May 3, 2016 6:51 PM
 # @Email:  tobias.jakobi@med.uni-heidelberg.de
 # @Project: University Hospital Heidelberg, Section of Bioinformatics and Systems Cardiology
-# @Last modified by:   tjakobi
-# @Last modified time: Wednesday, May 4, 2016 11:53 AM
 # @License: CC BY-NC-SA
 
 #SBATCH -n 1
 #SBATCH -N 1
-#SBATCH -c 20
-#SBATCH --mem=4G
-#SBATCH -J "flexbar single"
-
+#SBATCH -c 15
+#SBATCH --mem=20G
+#SBATCH -J "flexbar paired"
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT_80
+#SBATCH --mail-user=tobias.jakobi@med.uni-heidelberg.de
 
 # check if we have 3 arguments
 if [ ! $# == 2 ]; then
-  echo "Usage: $0 [Read file] [target dir e.g. /tmp/]"
+  echo "Usage: $0 [Read 1 file] [target dir e.g. /tmp/]"
   exit
 fi
 
-# $1 -> Read file
+# $1 -> Read 1
 # $2 -> Target directory
+
 
 # remove the file extension and potential "R1" markings
 # (works for double extension, e.g. .fastq.gz)
-target=`expr ${1/_R1/} : '\(.*\)\..*\.'`
+target=`expr ${1//} : '\(.*\)\..*\.'`
 
 # load the flexbar module
 module load flexbar
 
-# run on 20 CPUs
+# run on 10 CPUs
 # compress with bz2
 # only 30nt or longer
 # no uncalled bases
 # quality min phred 28
 # use sanger quality values (i.e. Illumina 1.9+ encoding)
-flexbar -r $1 -t $2/$target  -n 20 -z BZ2 -m 30 -u 0 -q 28 -a /biosw/flexbar/Adapter.fa -f sanger
+#flexbar -r $1 -p $2 -t $3/$target  -n 20 -z GZ -m 30 -u 0 -qt 28 -a /biosw/flexbar/Adapter.fa -qf sanger
+#flexbar -r $1 -p $2 -t $3/$target  -n 20 -z GZ -m 30 -u 0 -q 28 -a /biosw/flexbar/Adapter.fa -f sanger -j 
+flexbar -r $1 -t $2/$target -n 15 -z GZ -m 30 -u 0  -q TAIL -qt 28 -as "AGATCGGAAGAG" -qf sanger -j
+#flexbar -r $1 -p $2 -t $3/$target  -n 20 -z GZ -m 30 -u 0  -q TAIL -qt 28 -a /biosw/flexbar/Adapter.fa -qf sanger -j
