6512a166f138fc2b08bfd646bf67be026b26fb17
Tobias Jakobi <tobias.jakobi@med.uni-heidelberg.de>
2019-07-12 10:53:01 +0200

    Adding GPU-optimized slurm submission script for guppy (3.1.5)

---
+++ slurm_guppy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# @Author: Tobias Jakobi <tjakobi>
+# @Email:  tobias.jakobi@med.uni-heidelberg.de
+# @Project: University Hospital Heidelberg, Section of Bioinformatics and Systems Cardiology
+# @License: CC BY-NC-SA
+
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 40 
+#SBATCH --mem=200G
+#SBATCH -J "guppy basecaller"
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT_80
+#SBATCH --mail-user=tobias.jakobi@med.uni-heidelberg.de
+#SBATCH -p gpu
+
+module load guppy
+
+# check if we have 2 arguments
+if [ ! $# == 4 ]; then
+  echo "Usage: $0 [Input folder (recursive)] [Output folder] [Kit] [Flow cell type]"
+  exit
+fi
+
+guppy_basecaller -r -i ${1} -s ${2} --flowcell ${4} --kit ${3} --num_callers 4 --gpu_runners_per_device 3 --cpu_threads_per_caller 1 --device "cuda:all"
